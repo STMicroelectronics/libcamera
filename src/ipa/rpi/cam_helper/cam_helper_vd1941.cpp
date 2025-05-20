a4c47dafc4feb66cf07ed8d94825a95cf956cf0d
Benjamin Mugnier <benjamin.mugnier@foss.st.com>
2025-05-20 14:32:06 +0200

    ipa: rpi: Fix VD1941 cam_helper case
    Signed-off-by: Benjamin Mugnier <benjamin.mugnier@foss.st.com>

--- src/ipa/rpi/cam_helper/cam_helper_vd1941.cpp
+++ src/ipa/rpi/cam_helper/cam_helper_vd1941.cpp
@@ -15,10 +15,10 @@ using namespace RPiController;
 using libcamera::utils::Duration;
 using namespace std::literals::chrono_literals;
 
-class CamHelpervd1941 : public CamHelper
+class CamHelperVd1941 : public CamHelper
 {
 public:
-	CamHelpervd1941();
+	CamHelperVd1941();
 	uint32_t gainCode(double gain) const override;
 	double gain(uint32_t gainCode) const override;
 	uint32_t exposureLines(const Duration exposure, const Duration lineLength) const override;
@@ -47,32 +47,32 @@ private:
 };
 
 /*
- * vd1941 currently doesn't output metadata, so we have to use the "unicam parser"
+ * VD1941 currently doesn't output metadata, so we have to use the "unicam parser"
  * which works by counting frames.
  */
 
-CamHelpervd1941::CamHelpervd1941()
+CamHelperVd1941::CamHelperVd1941()
 	: CamHelper({}, frameIntegrationDiff)
 {
 }
 
-uint32_t CamHelpervd1941::gainCode(double gain) const
+uint32_t CamHelperVd1941::gainCode(double gain) const
 {
 	return static_cast<uint32_t>(16.0 - 16.0 / gain);
 }
 
-double CamHelpervd1941::gain(uint32_t gainCode) const
+double CamHelperVd1941::gain(uint32_t gainCode) const
 {
 	return 16.0 / (16.0 - static_cast<double>(gainCode));
 }
 
-uint32_t CamHelpervd1941::exposureLines(const Duration exposure,
+uint32_t CamHelperVd1941::exposureLines(const Duration exposure,
 					[[maybe_unused]] const Duration lineLength) const
 {
 	return std::max<uint32_t>(minExposureLines, exposure / timePerLine);
 }
 
-Duration CamHelpervd1941::exposure(uint32_t exposureLines,
+Duration CamHelperVd1941::exposure(uint32_t exposureLines,
 				   [[maybe_unused]] const Duration lineLength) const
 {
 	return std::max<uint32_t>(minExposureLines, exposureLines) * timePerLine;
@@ -80,7 +80,7 @@ Duration CamHelpervd1941::exposure(uint32_t exposureLines,
 
 static CamHelper *create()
 {
-	return new CamHelpervd1941();
+	return new CamHelperVd1941();
 }
 
 static RegisterCamHelper reg("vd1941", &create);
