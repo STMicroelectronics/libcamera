b8679225ea17215ad5355c435f1d8f92a8e35890
Sylvain Petinot <sylvain.petinot@foss.st.com>
2025-06-25 08:54:28 +0200

    Rename vd1941 to vd1943
    Mass-market name for Maneki is 'vd1943' (RGBIr variant) and 'vd5943'
    (Mono variant).
    
    Signed-off-by: Sylvain Petinot <sylvain.petinot@foss.st.com>

--- src/ipa/rpi/cam_helper/cam_helper_vd1943.cpp
+++ src/ipa/rpi/cam_helper/cam_helper_vd1943.cpp
@@ -2,7 +2,7 @@
 /*
  * Copyright (C) STMicroelectronics SA 2024
  *
- * Camera information for vd1941 sensor
+ * Camera information for vd1943 sensor
  */
 
 #include <assert.h>
@@ -11,10 +11,10 @@
 
 using namespace RPiController;
 
-class CamHelperVd1941 : public CamHelper
+class CamHelperVd1943 : public CamHelper
 {
 public:
-	CamHelperVd1941();
+	CamHelperVd1943();
 	uint32_t gainCode(double gain) const override;
 	double gain(uint32_t gainCode) const override;
 
@@ -30,28 +30,28 @@ private:
 };
 
 /*
- * VD1941 currently doesn't output metadata, so we have to use the "unicam parser"
+ * VD1943 currently doesn't output metadata, so we have to use the "unicam parser"
  * which works by counting frames.
  */
 
-CamHelperVd1941::CamHelperVd1941()
+CamHelperVd1943::CamHelperVd1943()
 	: CamHelper({}, frameIntegrationDiff)
 {
 }
 
-uint32_t CamHelperVd1941::gainCode(double gain) const
+uint32_t CamHelperVd1943::gainCode(double gain) const
 {
 	return static_cast<uint32_t>(16.0 - 16.0 / gain);
 }
 
-double CamHelperVd1941::gain(uint32_t gainCode) const
+double CamHelperVd1943::gain(uint32_t gainCode) const
 {
 	return static_cast<double>(16.0 / (16 - gainCode));
 }
 
 static CamHelper *create()
 {
-	return new CamHelperVd1941();
+	return new CamHelperVd1943();
 }
 
-static RegisterCamHelper reg("vd1941", &create);
+static RegisterCamHelper reg("vd1943", &create);
