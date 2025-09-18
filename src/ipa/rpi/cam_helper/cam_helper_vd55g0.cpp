ae124e29d811d090e6e09d91fc1749a2b7ac3c40
Benjamin Mugnier <benjamin.mugnier@foss.st.com>
2025-09-18 11:38:26 +0200

    Support vd55g0
    Signed-off-by: Benjamin Mugnier <benjamin.mugnier@foss.st.com>

---
+++ src/ipa/rpi/cam_helper/cam_helper_vd55g0.cpp
@@ -0,0 +1,54 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) STMicroelectronics SA 2025
+ *
+ * Camera information for vd55g0 sensor
+ */
+
+#include <assert.h>
+
+#include "cam_helper.h"
+
+using namespace RPiController;
+
+class CamHelperVd55g0 : public CamHelper
+{
+public:
+	CamHelperVd55g0();
+	uint32_t gainCode(double gain) const override;
+	double gain(uint32_t gainCode) const override;
+
+private:
+	/*
+	 * Smallest difference between the frame length and integration time,
+	 * in units of lines.
+	 */
+	static constexpr int frameIntegrationDiff = 64;
+};
+
+/*
+ * OV9281 doesn't output metadata, so we have to use the "unicam parser" which
+ * works by counting frames.
+ */
+
+CamHelperVd55g0::CamHelperVd55g0()
+	: CamHelper({}, frameIntegrationDiff)
+{
+}
+
+uint32_t CamHelperVd55g0::gainCode(double gain) const
+{
+	return static_cast<uint32_t>(32.0 - 32.0 / gain);
+}
+
+double CamHelperVd55g0::gain(uint32_t gainCode) const
+{
+	return static_cast<double>(32.0 / (32 - gainCode));
+}
+
+static CamHelper *create()
+{
+	return new CamHelperVd55g0();
+}
+
+static RegisterCamHelper reg("vd55g0", &create);
